aa108b1660f3ed0523be86ff483c81b70168ed07
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-07 18:05:24 +0100

    [add]: Home component (pages)

---
+++ src/pages/Home.vue
@@ -0,0 +1,27 @@
+<script>
+
+  export default {
+
+    name: 'Home',
+
+    data() {
+      return {}
+    },
+
+    methods: {},
+    computed: {},
+    mounted() {}
+    
+  }
+
+</script>
+
+<template>
+
+  <h1>Home Page</h1>
+
+</template>
+
+<style>
+
+</style>
