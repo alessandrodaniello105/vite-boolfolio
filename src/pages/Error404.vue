634db94e4e9d789bf9fbfdadb003e970e6908848
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-11 00:57:04 +0100

    [add]: error404 page+route

---
+++ src/pages/Error404.vue
@@ -0,0 +1,30 @@
+<script>
+
+  export default {
+
+    name: 'Error404',
+
+    data() {
+      return {}
+    },
+
+    methods: {},
+    computed: {},
+    mounted() {}
+    
+  }
+
+</script>
+
+<template>
+
+  <div class="text-center ">
+    <h1 class="text-danger">Errore 404</h1> 
+    <p>Pagina non trovata</p>
+  </div>
+
+</template>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Projects from '../pages/Projects.vue'
 import ProjectDetail from '../pages/ProjectDetail.vue'
 import About from '../pages/About.vue'
 import Contacts from '../pages/Contacts.vue'
+import Error404 from '../pages/Error404.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -33,6 +34,11 @@ const router = createRouter({
       path: '/contatti',
       name: 'contacts',
       component: Contacts
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'error404',
+      component: Error404
     }
     
   ]
