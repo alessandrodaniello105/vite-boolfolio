7b880f743c0a8520fca65f506abff8efd4d55f09
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-11 00:45:49 +0100

    [add]: ProjectDetail page + getPost method

---
+++ src/pages/ProjectDetail.vue
@@ -0,0 +1,51 @@
+<script>
+  import axios from 'axios'
+  import { store } from '../data/store.js'
+
+  export default {
+
+    name: 'ProjectDetail',
+
+    data() {
+      return {
+        project: {}
+      }
+    },
+
+
+    methods: {
+      getProject(slug) {
+        axios.get(store.apiUrl + 'projects/' + slug)
+          .then(res => {
+            this.project = res.data;
+          })
+      }
+    },
+
+    computed: {
+      technologiesList() {
+        return this.project.technologies?.map(technology => technology.name).join(', ') || 'Nessuna categoria';
+      }
+    },
+
+    mounted() {
+      this.getProject(this.$route.params.slug);
+    }
+    
+  }
+
+</script>
+
+<template>
+
+  <div class="card">
+    <h2>{{ project.title }}</h2>
+    <p>Descrizione: {{ project.description }}</p>
+    <p>Tipo: {{ project.type?.name ?? 'Nessun tipo' }} | {{ technologiesList }}</p>
+  </div>
+
+</template>
+
+<style>
+
+</style>
