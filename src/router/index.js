348adedb18059497ae0c6fca2329f6654e38de30
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-11 00:44:49 +0100

    [add]: ProjectDetail+About+Contacts in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '../pages/Home.vue'
 import Projects from '../pages/Projects.vue'
+import ProjectDetail from '../pages/ProjectDetail.vue'
+import About from '../pages/About.vue'
+import Contacts from '../pages/Contacts.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,6 +19,21 @@ const router = createRouter({
       name: 'projects',
       component: Projects
     },
+    {
+      path: '/progetti/:slug',
+      name: 'project-detail',
+      component: ProjectDetail
+    },
+    {
+      path: '/chi-sono',
+      name: 'about',
+      component: About
+    },
+    {
+      path: '/contatti',
+      name: 'contacts',
+      component: Contacts
+    }
     
   ]
 })
