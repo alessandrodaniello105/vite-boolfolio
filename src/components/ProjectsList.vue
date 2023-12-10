4a997cfe3f9e10cdf16ae821fe93dee954e1f4ea
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-11 00:44:11 +0100

    [add]: project show router-link in ProjectsList

---
+++ src/components/ProjectsList.vue
@@ -0,0 +1,46 @@
+<script>
+  import {store} from '../data/store.js'
+
+
+  export default {
+
+    name: 'ProjectComponent',
+
+    data() {
+      return {
+        store
+      }
+    },
+
+    methods: {},
+    computed: {},
+    mounted() {}
+    
+  }
+
+</script>
+
+<template>
+
+  <h1>Lista Progetti</h1>
+  <ul class="projects-list">
+    
+    <li v-for="project in store.projects" :key="project.id">
+      <router-link :to="{name:'project-detail', params:{slug: project.slug}}"><span>{{project.title}}</span></router-link>
+    </li>
+
+    
+  </ul>
+
+</template>
+
+<style lang="scss" scoped>
+
+ul.projects-list {
+  margin: 20px 0;
+  li {
+    margin-bottom: 5px;
+  }
+}
+
+</style>
