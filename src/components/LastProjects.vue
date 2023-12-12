90623f83d5970774bbbb9d5cb29879cba3b4a8c6
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-12 15:18:44 +0100

    [add]: now projects list is prop in LastProjects

--- src/components/LastProjects.vue
+++ src/components/LastProjects.vue
@@ -1,15 +1,15 @@
 <script>
-  import {store} from '../data/store.js'
-
 
   export default {
 
     name: 'LastProjects',
 
     data() {
-      return {
-        store
-      }
+      return {}
+    },
+
+    props: {
+      list: Array
     },
 
     methods: {},
@@ -24,8 +24,20 @@
 
   <h1>Ultimi Progetti</h1>
   <div class="projects-wrapper">
-    <div class="project" v-for="project in store.projects" :key="project.id">
+    <div class="project" v-for="project in list" :key="project.id">
       <h1>{{ project.title }}</h1>
+      
+      <div class="img-box">
+        <img class="img-thumbnail" :src="project.image" alt="">
+      </div>
+
+      <div class="text-box">
+        <p>{{ project.description }}</p>
+
+        //TODO: migliorare visualizzazione link
+        <span>Link al progetto: <a :href="project.link ?? '#'">{{ project.link ?? 'Nessun link al progetto' }}</a> </span>
+        <!-- <span v-else>Link al progetto:  </span> -->
+      </div>
     </div>
   </div>
 
@@ -33,10 +45,31 @@
 
 <style lang="scss" scoped>
 
-ul.projects-list {
-  margin: 20px 0;
-  li {
-    margin-bottom: 5px;
+.project {
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  margin-bottom: 75px;
+  h1 {
+    font-style: italic;
+    color: var(--text-headline);
+  }
+  .img-box {
+    align-self: center;
+    max-width: 900px;
+    img {
+      object-position: center;
+      width: 100%;
+    }
+  }
+  .text-box {
+    color: var(--text-paragraph);
+    p {
+      font-size: 2rem;
+    }
+    span {
+      font-size: 1rem;
+    }
   }
 }
 
