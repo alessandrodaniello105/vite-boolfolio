c1348cfca65a9e2785a3a0a0450974c1719dc522
alessandrodaniello105 <kanymsevement@gmail.com>
2023-12-12 16:27:48 +0100

    [edit]: img-box moved in container (About.vue)

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -20,11 +20,11 @@
 
   <h1 class="title">Chi sono</h1>
 
-  <div class="img-box">
-    <img src="/public/imgs/jumbotron.jpg" alt="">
-  </div>
-
+  
   <div class="container">
+    <div class="img-box">
+      <img src="/public/imgs/jumbotron.jpg" alt="">
+    </div>
     <div class="text-box">
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure rerum voluptas ducimus itaque ipsam inventore iste maiores molestiae voluptatibus dolorem. Facere iusto sint adipisci optio asperiores harum incidunt tempora earum at vitae provident velit eligendi veniam deleniti, ad eaque dolorum voluptates recusandae. Quo soluta ratione nobis officiis, a labore aliquam voluptatum ut quod eaque eos dicta officia incidunt reiciendis vitae fuga dolore nesciunt quia quam velit amet. Saepe non quae eos numquam autem soluta repellendus, alias recusandae sapiente ipsa tempore, totam illo deleniti sequi voluptas consequuntur rerum veritatis. Numquam repudiandae nam molestias nisi natus ullam, aspernatur ducimus assumenda excepturi expedita voluptates fugiat quaerat, illum obcaecati minus tenetur vel recusandae harum reiciendis quis qui. Odit assumenda reiciendis dolorum sint mollitia, labore quae, amet maiores inventore voluptatem autem. Ipsam delectus nam veritatis iste sunt, provident natus quos officia dolorum illum autem voluptatum reiciendis libero voluptas ullam labore accusamus nostrum incidunt voluptatem cumque non optio ea quaerat explicabo? Ipsum autem ut minus voluptas nisi. Ut ipsam quod perferendis hic inventore. Quidem doloribus voluptatibus aut provident perferendis dolorum quam maiores deleniti asperiores, sit rerum excepturi hic nobis veniam delectus itaque rem dolorem fugit blanditiis unde repudiandae aliquid voluptatem totam reiciendis! Esse omnis saepe distinctio.
